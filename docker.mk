59cf0a7e2c3ae140fad8c7f26e85adec3d85aa10
Gary Rutland <gary.rutland@sainsburys.co.uk>
2025-04-09 14:54:46 +0100

    Update docker.mk

--- docker.mk
+++ docker.mk
@@ -4,8 +4,6 @@ ifeq (, $(shell which docker))
 $(error "Executable 'docker' not found in PATH")
 endif
 
-DKR_MAKEFILE_DIR := $(abspath $(dir $(filter %/docker.mk,$(MAKEFILE_LIST))))
-
 dkr_version:
 	docker --version
 
