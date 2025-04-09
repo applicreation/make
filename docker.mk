9eee3a6a510304a4d2d98d51ce8740b41cee132f
Gary Rutland <gary.rutland@dae.mn>
2025-04-09 14:37:56 +0100

    Update docker.mk

--- docker.mk
+++ docker.mk
@@ -8,3 +8,7 @@ DKR_MAKEFILE_DIR := $(abspath $(dir $(filter %/docker.mk,$(MAKEFILE_LIST))))
 
 dkr_version:
 	docker --version
+
+dkr_system_prune:
+	docker system prune -fa --volumes
+	docker volume prune -f
