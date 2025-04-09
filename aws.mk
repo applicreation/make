91d7cd9b522f8fc66276a18724b4cdf6e4b76ae2
Gary Rutland <gary.rutland@sainsburys.co.uk>
2025-04-09 09:17:55 +0100

    Update aws.mk

--- aws.mk
+++ aws.mk
@@ -4,10 +4,6 @@ ifndef AWS_PROFILE
 $(error "AWS_PROFILE is not set")
 endif
 
-ifndef AWS_ACCOUNT_ID
-$(error "AWS_ACCOUNT_ID is not set")
-endif
-
 ifndef AWS_REGION
 $(error "AWS_REGION is not set")
 endif
