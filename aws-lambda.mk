444af6cf09bfdbe59e15145762857cb840196f71
Gary Rutland <gary.rutland@dae.mn>
2025-04-10 14:58:22 +0100

    Update aws-lambda.mk

--- aws-lambda.mk
+++ aws-lambda.mk
@@ -1,4 +1,8 @@
+lambda_%: export NAME += -$(subst _,-,$(notdir $(CURDIR)))
+lambda_%: export DIR = .
+
 lambda_%: DOCKER_COMPOSE_FILE = $(CURDIR)/docker-compose.yaml
+
 lambda_%: DOCKER_COMPOSE_CMD = docker compose -f $(DOCKER_COMPOSE_FILE)
 
 lambda_build:
