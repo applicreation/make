a3029440c719b7acf567f28b650cc9e831a7bffa
Gary Rutland <gary.rutland@dae.mn>
2025-04-10 12:39:42 +0100

    Update git.mk

--- git.mk
+++ git.mk
@@ -4,11 +4,9 @@ ifeq (, $(shell which git))
 $(error "Executable 'git' not found in PATH")
 endif
 
-ifeq (, $(wildcard $(GIT_DIR)))
-$(error "No git repository found in $(GIT_DIR)")
+GIT_BRANCH ?= $(shell git rev-parse --abbrev-ref HEAD 2>/dev/null)
+GIT_TAG ?= $(shell git rev-parse --short=10 HEAD 2>/dev/null)
+
+ifeq ($(GIT_BRANCH),)
+$(error "No git repository found")
 endif
-
-GIT_DIR ?= $(PWD)/.git
-
-GIT_BRANCH ?= $(shell git rev-parse --abbrev-ref HEAD)
-GIT_TAG ?= $(shell git rev-parse --short=10 HEAD)
