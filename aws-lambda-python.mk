2c99c679374fcd857557d0e4502b23b71ab9e27f
Gary Rutland <gary.rutland@dae.mn>
2025-04-10 17:00:22 +0100

    Removed unfinished files

--- aws-lambda-python.mk
+++
@@ -1,8 +0,0 @@
-lambda_update: lambda_build
-	docker compose run --rm dev pip-compile ./src/lambda_function/requirements.in
-
-lambda_update_test: lambda_build
-	docker compose run --rm dev pip-compile ./tests/requirements.in
-
-lambda_test: lambda_build
-	docker compose run --rm dev pytest --cov-report=term-missing:skip-covered --cov=src/lambda_function $(JUNITXML) ./tests
