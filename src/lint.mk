b0c77c368ad78bc213aa693d2da21d654fb7b907
Gary Rutland <gary.rutland@dae.mn>
2025-04-15 20:55:35 +0100

    Added lint prettier

--- src/lint.mk
+++ src/lint.mk
@@ -6,6 +6,8 @@ LINT_IMAGE ?= ghcr.io/super-linter/super-linter:$(LINT_VERSION)
 LINT_BRANCH ?= main
 LINT_DIRECTORY ?= $(CURDIR)
 
+LINT_PRETTIER_FILE ?= .
+
 LINT_CMD = docker run --rm --platform linux/amd64 \
 	-e SHELL=/bin/bash \
 	-e RUN_LOCAL=true \
@@ -16,3 +18,9 @@ LINT_CMD = docker run --rm --platform linux/amd64 \
 
 lint:
 	$(LINT_CMD)
+
+lint_prettier_check:
+	docker run -w /tmp/prettier -v $(PWD):/tmp/prettier node:22-alpine npx prettier --check $(LINT_PRETTIER_FILE)
+
+lint_prettier_write:
+	docker run -w /tmp/prettier -v $(PWD):/tmp/prettier node:22-alpine npx prettier --write $(LINT_PRETTIER_FILE)
